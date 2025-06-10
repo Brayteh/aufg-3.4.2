50d492546bd1a331819fa450259079c792843b78
Brayteh <ya3koub5@gmail.com>
2025-06-10 11:31:56 +0200

    Test 2

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 void main() {
- int age = 15;
- bool hasParentalConsent = true;
+ int age = 13;
+ bool hasParentalConsent = false;
  int movieAgeRating = 16;
  if (age >= movieAgeRating || (hasParentalConsent && age >= movieAgeRating - 2)) { 
     print("You can watch the movie.");
